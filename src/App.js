0924897e0a72a77f626109fb1d80fd1135bab556
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-10-12 00:27:14 -0300

    pulling weather data from API

--- src/App.js
+++ src/App.js
@@ -3,27 +3,57 @@ import axios from 'axios'
 
 function App () {
   const [location, setLocation] = useState(false)
+  const [weather, setWeather] = useState(false)
+
+  const getWeather = async (lat, long) => {
+    const result = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric',
+      },
+    })
+    setWeather(result.data)
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude)
-      console.log(position.coords.longitude)
+      getWeather(position.coords.latitude, position.coords.longitude)
       setLocation(true)
     })
   }, [])
 
-  return (
-    <>
-      <h3>Clima nas suas coordenadas</h3>
-      <ul>
-        <li>Temperatura atual: x°</li>
-        <li>Temperatura máxima: x°</li>
-        <li>Temperatura mínima: x°</li>
-        <li>Pressão: x hpa</li>
-        <li>Umidade: x%</li>
-      </ul>
-    </>
-  )
+  if (location === false) {
+    return (
+      <>
+        Voce precisa habilitar a localização no browser
+      </>
+    )
+  } else if (weather === false) {
+    return (
+      <>
+        <h3>Carregando o clima</h3>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h3>Clima nas suas coordenadas({weather.weather[0].description})</h3>
+        <ul>
+          <li>Temperatura atual: {weather.main.temp}°</li>
+          <li>Temperatura máxima: {weather.main.temp_max}°</li>
+          <li>Temperatura mínima: {weather.main.temp_min}°</li>
+          <li>Pressão: {weather.main.pressure}hpa</li>
+          <li>Umidade: {weather.main.humidity}%</li>
+        </ul>
+      </>
+    )
+  }
 }
 
 export default App
+/*
+
+*/
